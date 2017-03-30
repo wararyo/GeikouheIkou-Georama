7008bb640995b81e386505445599f5f6ce0c3b6d
wararyo <wararyo@gmail.com>
2017-03-30 18:49:05 +0900

    Initial Commit

---
+++ GeikouheIkou-Georama/Assets/MainManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using wararyo.EclairCueMaker;
+using UnityEngine.UI;
+
+public class MainManager : MonoBehaviour {
+
+	public CueScenePlayer cueScenePlayer;
+	public TimeCounter timeCounter;
+	public Text remainTimeText;
+
+	public CueScene cueSceneStart;
+	public CueScene cueSceneTimeUp;
+
+	// Use this for initialization
+	void Start () {
+		timeCounter.OnTime += OnTimeCounterTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			cueScenePlayer.Play (cueSceneStart);
+		}
+		if (timeCounter.isCounting) {
+			remainTimeText.text = timeCounter.RemainTimeString;
+		}
+	}
+
+	private void OnTimeCounterTime(){
+		cueScenePlayer.Play (cueSceneTimeUp);
+	}
+}
---
+++ GeikouheIkou-Georama/Assets/TimeCounter.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using wararyo.EclairCueMaker;
+using System;
+
+public class TimeCounter : CueEventBase {
+
+	public string RemainTimeString {
+		get{
+			return RemainTime.ToString ("F2");
+		}
+	}
+
+	public float RemainTime{
+		get{
+			return Mathf.Clamp(defaultTime - (Time.realtimeSinceStartup - whenStarted),0,defaultTime);
+		}
+	}
+
+	public System.Action OnTime;
+
+	public float defaultTime = 60;
+
+	private float whenStarted = 0;
+
+	[System.NonSerialized]
+	public bool isCounting = false;
+
+	public override string EventName
+	{
+		get
+		{
+			return "Start TimeCounter";
+		}
+	}
+
+	public override string EventID
+	{
+		get
+		{
+			return "gjc66rgy9awr33r94tjopy5";
+		}
+	}
+
+	public override Type ParamType
+	{
+		get
+		{
+			return typeof(void);
+		}
+	}
+
+
+	public override void Cue(object param)
+	{
+		//ここに任意の処理を記述
+		whenStarted = Time.realtimeSinceStartup;
+		isCounting = true;
+	}
+
+	public void Update(){
+		if (RemainTime <= 0) {
+			isCounting = false;
+			OnTime ();
+		}
+	}
+
+}
