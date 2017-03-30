af6992afdf58a379560c4ea289bbdb01ccdcf3b1
wararyo <wararyo@gmail.com>
2017-03-30 22:00:10 +0900

    Wiiリモコンの回転に合わせてモデルも回転させたかった

--- GeikouheIkou-Georama/Assets/WiimoteRotate.cs
+++ GeikouheIkou-Georama/Assets/WiimoteRotate.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using WiimoteApi;
 
 public class WiimoteRotate : MonoBehaviour {
 
+	Wiimote wiimote;
+
 	// Use this for initialization
 	void Start () {
-	
+		if (wiimote == null) {
+			Debug.Log ("hote");
+			if (WiimoteManager.FindWiimotes ()) {
+				wiimote = WiimoteManager.Wiimotes [0];
+				Debug.Log ("Wiimote Found");
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (WiimoteManager.HasWiimote ()) {
+			//transform.rotation = Quaternion.LookRotation(new Vector3(wiimote.MotionPlus.YawSpeed,wiimote.MotionPlus.PitchSpeed,wiimote.MotionPlus.RollSpeed));
+
+			int ret = wiimote.ReadWiimoteData ();
+
+			if (ret > 0 && wiimote.current_ext == ExtensionController.MOTIONPLUS) {
+				Vector3 offset = new Vector3 (-wiimote.MotionPlus.PitchSpeed,
+					                 wiimote.MotionPlus.YawSpeed,
+					                 wiimote.MotionPlus.RollSpeed) / 95f; // Divide by 95Hz (average updates per second from wiimote)
+			
+				transform.Rotate (offset);
+			}
+		}
 	}
 }
